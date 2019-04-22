37741f8093a3111b2d8599eb0c249a0773a2638c
Timothy Warnock <tim@timwarnock.com>
2019-04-21 21:19:19 -0700

    down and dirty prototype

--- x404/x404.py
+++ x404/x404.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python
+''' Usage: x404.py command [options]
+
+    Where command includes:
+    add [url] * e.g., add http://www.anattatechnologies.com/
+    get [key] * e.g., get esza (returns the matching url)
+    del [key] * e.g., del esza (removes the matching entry)
+    list      * lists all entries
+
+'''
+import os
+import sys
+import math
+import sqlite3
+
+
+def add_url(url):
+  conn = sqlite3.connect("URLs.db")
+  curs = conn.cursor()
+  curs.execute("select count(rowid) from URLS")
+  numrows = curs.fetchall()[0][0]
+  short_url = getalpha(numrows)
+  curs.execute("insert into URLS(short_url, long_url) values(?,?)",(short_url,url))
+  conn.commit()
+  conn.close()
+  return short_url
+
+
+
 def revb(n):
   min_digits = 1 + int(math.log(n,16))
   bit_size = 4 * min_digits
@@ -18,22 +47,6 @@ def mixb(n):
    return int(reverse_number,2)
 
 
-
-top16pos = 'etaoinshrdlcumwf'
-top16neg = 'ofsrdanluetmcwhi'
-def num16alpha(num):
-    chars = []
-    toggle = 0
-    while num > 0:
-        num, d = divmod(num, 16)
-        if toggle % 2 == 0:
-          chars.append(top16pos[d])
-        else:
-          chars.append(top16neg[d])
-        toggle += 1
-    return ''.join(reversed(chars))
-
-
 top16pos = 'etaoinshrdlcumwf'
 def num16alpha(num):
     chars = []
@@ -43,8 +56,8 @@ def num16alpha(num):
     return ''.join(reversed(chars))
 
 
-def geta(n):
-  return num16alpha(mixb(n,5))
+def getalpha(n):
+  return num16alpha(mixb(n))
 
 
 '''
@@ -66,5 +79,21 @@ def getaC(rmax):
       break
     else:
       c[geta(i)] = i
+
+
+
+
+
+if __name__ == '__main__':
+  ''' optparse? '''
+  if len(sys.argv) > 2 and sys.argv[1] == "add":
+    short_url = add_url(sys.argv[2])
+    while os.path.exists("../%s" % (short_url)):
+      print >> sys.stderr, "%s already exists and won't cause a 404" % (short_url)
+      short_url = add_url(sys.argv[2])
+    print "SUCCESS\nhttp://avant.net/%s" % (short_url)
+      
+
+
 
 
