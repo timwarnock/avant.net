7e3eadd72eaac71e459d00e5be20a19a2c95cb56
Timothy Warnock <tim@timwarnock.com>
2019-05-01 09:07:07 -0700

    updates

--- x404/index.php
+++ x404/index.php
@@ -7,23 +7,30 @@
     with deadman redirect (html redirect, disabled by JavaScript, noscript warning)
 
  2. JavaScript create psuedo form
-    XHR->POST(URL, key)
-      verify key is sha1(IP+URL)
-      save in sqlite3: URL, timestamp, IP, sha1(IP+UA+handshake+key)
-      respond with handshake
-    http redirect (with key,URL stored in sessionStorage), and ?handshake=...
+      psuedo form includes form-key (time-sensitive)
+    XHR->POST(form-key, URL, request-key)
+      verify request-key is sha256(IP+URL)
+      verify form-key
+      save in sqlite3: IP, URL, timestamp, sha256(UA+handshake+half(key))
+      respond with handshake (UUID)
+    http redirect (with request-key, URL stored in sessionStorage), and ?handshake=...
 
  3. /x404/?handshake=...
-    retrieve key from sessionStorage
-    WAIT 1-second
-    XHR->PUT(sha1(IP+UA+handshake+key))
-      IF server time-delta is > 1.1s and <11s AND IP == db(IP)
-      then commit URL, and cleanup
-      => respond with short_url
-    http redirect ?short_url=...
+      psuedo form includes form-key (time-sensitive)
+      retrieve URL, request-key from sessionStorage
+      WAIT 1-second
+    XHR->PUT(form-key, sha256(UA+handshake+half(request-key)))
+      verify form-key
+      verify IP == db(IP)
+      verify time-delta is > 1.01s and <11s
+      verify sha256(UA+handshake+half(key)) == db(sha256(...))
+      commit URL, and cleanup
+      => respond with urlid
 
- 4. "Congratulations"
-    provide option to resubmit (go to step 2, fill URL from sessionStorage)
+ 4. /x404/?urlid=...
+    "Congratulations"
+    provide option to resubmit? (go to step 2, fill URL from sessionStorage)
+    show novelty URLs (unicode, large-base numbers)
 
  *. ELSE "are you a robot?"
     Let's confirm you're a human (go to step 2, fill URL from sessionStorage)
