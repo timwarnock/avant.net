af36c57447430abf083d1dc072508e2a6dd517a5
Timothy Warnock <tim@timwarnock.com>
2019-05-05 08:16:22 -0700

    prototpye php interface into x404.db

--- x404/handler.php
+++ x404/handler.php
@@ -1,24 +1,87 @@
 <?php
+# vim: set fileencoding=utf-8 tabstop=4 shiftwidth=4 autoindent smartindent:
+##
+#
+#
+#
+##
+
+
+# Set internal character encoding to UTF-8
+mb_internal_encoding("UTF-8");
+
+# set the path to the db
+$DATABASE_PATH = 'sqlite:x404.db';
+
+#
+# supported encodings
+$_ENC = array(
+    'top16' => 'fwmucldrhsnioate',
+    'base62' => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    'greek' => 'ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω',
+    'greek' => 'ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω',
+    'anglosaxon' => 'ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ'
+);
+
+function _is_enc($nstr, $enc) {
+    foreach (preg_split('//u', $nstr, null, PREG_SPLIT_NO_EMPTY) as $c) {
+        if (mb_strpos($enc,$c) === FALSE)
+            return FALSE;
+    }
+    return TRUE;
+}
+
+function _decode($nstr, $enc) {
+    $base = mb_strlen($enc);
+    $basem = 1;
+    $n = 0;
+    foreach (preg_split('//u', $nstr, null, PREG_SPLIT_NO_EMPTY) as $c) {
+        $n += mb_strpos($enc,$c) * $basem;
+        $basem = $basem * $base;
+    }
+    return $n;
+}
 
 # 
-$lookupkey = trim($_SERVER['REQUEST_URI'],'/');
+$rowid = 0;
+$newURL = FALSE;
+$lookupkey = urldecode(trim($_SERVER['REQUEST_URI'],'/'));
+if (strlen($lookupkey)>0) {
+    if ($lookupkey[0] == '-') {
+        $rowid = _decode(trim($lookupkey,'-'), $_ENC['base62']);
+    } elseif (_is_enc($lookupkey, $_ENC['top16'])) {
+        $rowid = _decode($lookupkey, $_ENC['top16']);
+    } elseif (_is_enc($lookupkey, $_ENC['base62'])) {
+        $rowid = _decode($lookupkey, $_ENC['base62']);
+    } elseif (_is_enc($lookupkey, $_ENC['greek'])) {
+        $rowid = _decode($lookupkey, $_ENC['greek']);
+    } elseif (_is_enc($lookupkey, $_ENC['anglosaxon'])) {
+        $rowid = _decode($lookupkey, $_ENC['anglosaxon']);
+    }
+}
+
+
 
 # query URLS.db
-$dir = 'sqlite:URLs.db';
-$dbh  = new PDO($dir) or die("cannot open the database");
-$query =  $dbh->prepare("SELECT long_url from URLS where short_url = :short_url");
-$query->bindParam(':short_url', $lookupkey);
-$query->execute();
-$result = $query->fetchAll();
+if ($rowid > 0) {
+    $dbh  = new PDO($DATABASE_PATH) or die("cannot open the database");
+    $query =  $dbh->prepare("SELECT url FROM urls WHERE rowid= :rowid");
+    $query->bindParam(':rowid', $rowid);
+    $query->execute();
+    $result = $query->fetchAll();
+    if (count($result) > 0) {
+        $newURL = $result[0]['url'];
+    }
+}
 
-# see if there's a URL
-if (count($result) > 0 && substr($result[0]['long_url'], 0, strlen("http")) === "http") {
-  $newURL = $result[0]['long_url'];
+
+#print "yes<br> $lookupkey $rowid $newURL"; exit;
+
+if ($newURL) {
   header("Location:$newURL");
   exit;
 }
 
 http_response_code(404);
 print "404 Not Found";
-
 ?>
--- x404/index.php
+++ x404/index.php
@@ -1,44 +1,4 @@
 <?php
-/*
- * x404 - URL-shortener (via 404 handler)
- *
-
- 1. honeypot form
-    with deadman redirect (html redirect, disabled by JavaScript, noscript warning)
-
- 2. JavaScript create psuedo form
-      psuedo form includes form-key (time-sensitive)
-    XHR->POST(form-key, URL, request-key)
-      verify request-key is sha256(IP+URL)
-      verify form-key
-      save in sqlite3: IP, URL, timestamp, sha256(UA+handshake+half(key))
-      respond with handshake (UUID)
-    http redirect (with request-key, URL stored in sessionStorage), and ?handshake=...
-
- 3. /x404/?handshake=...
-      psuedo form includes form-key (time-sensitive)
-      retrieve URL, request-key from sessionStorage
-      WAIT 1-second
-    XHR->PUT(form-key, sha256(UA+handshake+half(request-key)))
-      verify form-key
-      verify IP == db(IP)
-      verify time-delta is > 1.01s and <11s
-      verify sha256(UA+handshake+half(key)) == db(sha256(...))
-      commit URL, and cleanup
-      => respond with urlid
-
- 4. /x404/?urlid=...
-    "Congratulations"
-    provide option to resubmit? (go to step 2, fill URL from sessionStorage)
-    show novelty URLs (unicode, large-base numbers)
-
- *. ELSE "are you a robot?"
-    Let's confirm you're a human (go to step 2, fill URL from sessionStorage)
-  
-
-*/
-
-
 
 
 ?>
